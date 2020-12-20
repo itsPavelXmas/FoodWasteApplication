610b437926b148b07b78098cf9d187cab892e8b5
itsPavelXmas <pavelcristian2399@gmail.com>
2020-12-20 23:58:27 +0200

    Commit again after resolving Conflicts hehehe

--- BE/src/database/connection.js
+++ BE/src/database/connection.js
@@ -1,15 +1,10 @@
 const Sequelize = require("sequelize");
-<<<<<<< HEAD
-
-const sequelize = new Sequelize('mysql://root:test@localhost:3306/FoodWasteDb');
-=======
 const config = require('../../config/config.json')
 const sequelize = new Sequelize(config.development.database,config.development.username,config.development.password,{
     host:config.development.host,
     port:config.development.port,
     dialect:config.development.dialect
 });
->>>>>>> 1d146c11b0e94c72385da64257f2f2df89b5819c
 
 module.exports = sequelize;
 global.sequelize = sequelize;
